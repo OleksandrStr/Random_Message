b325b7ee7e7f719b29ca2674b40c52637321dc6f
Oleksandr <92752633+OleksandrStr@users.noreply.github.com>
2021-10-22 01:22:24 +0200

    Made it better

--- RandomMessage.js
+++ RandomMessage.js
@@ -1,13 +1,25 @@
 
-Phrase1 = ["have a problem", "feel bad", "feel unmotivated"];
-Phrase2 = ["you are not alone", "you can do better", "it's not for always"];
-Phrase3 = ["so work harder", "so make a plan and take action", "so believe in yourself"];
+const Phrases = { 
+Phrase1: ["have a problem", "feel bad", "feel unmotivated"],
+Phrase2: ["you are not alone", "you can do better", "it's not for always"],
+Phrase3: ["so work harder", "so make a plan and take action", "so believe in yourself"]
+}
 
 const random = (num) => {
     return Math.floor(Math.random() * num.length);
 };
 
-console.log("Whenever you " + Phrase1[random(Phrase1)]);
-console.log("remember that " + Phrase2[random(Phrase2)]);
-console.log(Phrase3[random(Phrase3)]);
+const randomPhrase1 = "Whenever you " + Phrases.Phrase1[random(Phrases.Phrase1)];
+const randomPhrase2 = "remember that " + Phrases.Phrase2[random(Phrases.Phrase2)];
+const randomPhrase3 = Phrases.Phrase3[random(Phrases.Phrase3)];
+
+const finalPhrase = randomPhrase1 + "\n" + randomPhrase2 + "\n" + randomPhrase3;
+
+console.log(finalPhrase);
+
+
+
+
+
+
 
