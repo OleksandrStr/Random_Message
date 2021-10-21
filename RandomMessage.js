5302d9461af1466ef74dd209a3d720ce37d1d870
Oleksandr <92752633+OleksandrStr@users.noreply.github.com>
2021-10-22 00:46:58 +0200

    Modified random number function

--- RandomMessage.js
+++ RandomMessage.js
@@ -3,10 +3,11 @@ Phrase1 = ["have a problem", "feel bad", "feel unmotivated"];
 Phrase2 = ["you are not alone", "you can do better", "it's not for always"];
 Phrase3 = ["so work harder", "so make a plan and take action", "so believe in yourself"];
 
-const random = () => {
-    return Math.floor(Math.random() * 3);
+const random = (num) => {
+    return Math.floor(Math.random() * num.length);
 };
 
-console.log("Whenever you " + Phrase1[random()]);
-console.log("remember that " + Phrase2[random()]);
-console.log(Phrase3[random()]);
+console.log("Whenever you " + Phrase1[random(Phrase1)]);
+console.log("remember that " + Phrase2[random(Phrase2)]);
+console.log(Phrase3[random(Phrase3)]);
+
