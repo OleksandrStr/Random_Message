2a96ade19fb810d30c80377278d7ed466764956a
Oleksandr <92752633+OleksandrStr@users.noreply.github.com>
2021-10-22 00:17:19 +0200

    Added random number function

--- RandomMessage.js
+++ RandomMessage.js
@@ -3,5 +3,7 @@ Phrase1 = ["have a problem", "feel bad", "feel unmotivated"];
 Phrase2 = ["you are not alone", "you can do better", "it's not for always"];
 Phrase3 = ["so work harder", "so make a plan and take action", "so believe in yourself"];
 
-
+const random = () => {
+    return Math.floor(Math.random() * 3);
+};
 
